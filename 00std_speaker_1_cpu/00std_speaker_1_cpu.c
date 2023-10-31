57e875b30d8d2bfa0489e2555684f016169e89ba
Raphael <raphael.soupayavalliama@gmail.com>
2023-10-31 14:33:23 +0400

    modification des fichiers SEGGER lors du cour de Lundi

--- 00std_speaker_1_cpu/00std_speaker_1_cpu.c
+++ 00std_speaker_1_cpu/00std_speaker_1_cpu.c
@@ -1,19 +1,25 @@
 #include "nrf52833.h"
 
-void wait(void) {
+void wait(volatile uint32_t b) {
     volatile uint32_t a;
-    for (a=0; a<8000;a++);
+    for (a=0; a<b; a++);
 }
 
 int main(void) {
     
     // set speaker pin as output
     NRF_P0->PIN_CNF[0] = 0x00000003; // P0.00
-
+    volatile uint32_t b = 8000;
     while(1) {
         NRF_P0->OUTSET = (0x00000001 << 0); // speaker pin high
-        wait();
+        wait(b);
         NRF_P0->OUTCLR = (0x00000001 << 0); // speaker pin low
-        wait();
+        wait(b);
+
+        if (b==20000) {
+          b = 8000;
+        }else {
+          b = b + 100;
+        }
     }
 }
